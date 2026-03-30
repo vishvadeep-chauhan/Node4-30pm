a42189408a4fd5ef6132b6bbfb2bd77ceaf67e65
unknown <gdutt2304@gmail.com>
2026-03-30 15:38:57 +0530

    express first server

--- express/server.js
+++ express/server.js
@@ -1,9 +1,17 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("hello Avneesh");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  //   res.json({ message: "Welcome to the API" });
+  res.send("Welcome to the Home API");
+});
+app.get("/about", (req, res) => {
+  //   res.json({ message: "Welcome to the API" });
+  res.send("Welcome to the About API");
 });
 
-server.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
